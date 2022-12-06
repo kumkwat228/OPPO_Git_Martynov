2dad9fe7c07be0440e07bcdd021f8a7547571a50
Kirill Martynov <kirillmartynov517@gmail.com>
2022-12-06 13:06:05 +0700

    first version

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+double my_pow(double a, unsigned int n)
+{
+    double ch = a;
+    a = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        a *= ch;
+    }
+        return a;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a;
+    unsigned int n;
+    cout << "Ввод числа: \n";
+
+    while (cin >> a)
+    {
+        cout << "Ввод степени: \n";
+        cin >> n;
+        cout << endl << my_pow(a, n) << endl;
+        cout << "Ввод числа: \n";
+    }
+    return 0;
+}
+
