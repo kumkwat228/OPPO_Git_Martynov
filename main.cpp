1d09124c6c62b9b8f401450c614c5b710773e37f
Kirill Martynov <kirillmartynov517@gmail.com>
2022-12-06 13:09:17 +0700

    first vers

--- main.cpp
+++ main.cpp
@@ -3,32 +3,32 @@ using namespace std;
 
 double my_pow(double a, unsigned int n)
 {
-    double ch = a;
-    a = 1;
+	double ch = a;
+	a = 1;
 
-    for (int i = 0; i < n; i++)
-    {
-        a *= ch;
-    }
-        return a;
+	for (int i = 0; i < n; i++)
+	{
+		a *= ch;
+	}
+	return a;
 }
 
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
-    int a;
-    unsigned int n;
-    cout << "Ввод числа: \n";
+	double a;
+	unsigned int n;
+	cout << "Ввод числа: \n";
 
-    while (cin >> a)
-    {
-        cout << "Ввод степени: \n";
-        cin >> n;
-        cout << endl << my_pow(a, n) << endl;
-        cout << "Ввод числа: \n";
-    }
-    return 0;
+	while (cin >> a)
+	{
+		cout << "Ввод степени: \n";
+		cin >> n;
+		cout << endl << my_pow(a, n) << endl;
+		cout << "Ввод числа: \n";
+	}
+	return 0;
 }
 
